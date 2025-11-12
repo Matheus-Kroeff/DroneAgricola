3399c25be707a2d4ad8663c0d9fecea584d4f01d
Matheus Kroeff <matheuslckroeff@gmail.com>
2025-11-12 08:14:32 -0300

    Update DadoColetado.java

--- DadoColetado.java
+++ DadoColetado.java
@@ -1,15 +1,13 @@
-// incompleto
-
 public class DadoColetado {
 
-  private int id;
-  private int fkMissao; // Chave estrangeira no modelo OO, para referência à Missão
-  private String imagemURL;
-  private double temperatura; 
-  private double umidade;    
-  private double valorSensorSimulado;
+    private int id;
+    private int fkMissao; 
+    private String imagemURL;
+    private double temperatura;
+    private double umidade;
+    private double valorSensorSimulado;
 
-  public DadoColetado(int id, int fkMissao, String imagemURL, double temperatura, double umidade, double valorSensorSimulado) {
+    public DadoColetado(int id, int fkMissao, String imagemURL, double temperatura, double umidade, double valorSensorSimulado) {
         this.id = id;
         this.fkMissao = fkMissao;
         this.imagemURL = imagemURL;
@@ -18,20 +16,15 @@ public class DadoColetado {
         this.valorSensorSimulado = valorSensorSimulado;
     }
 
- public void registrarDado() {
+    public void registrarDado() {
         System.out.println("Dado ID " + this.id + " (Temp: " + this.temperatura + "°C) gerado pela Missão " + this.fkMissao + ".");
     }
     
-    // Getters
     public int getId() {
         return id;
     }
 
     public int getFkMissao() {
         return fkMissao;
-    }
-
-    public double getTemperatura() {
-        return temperatura;
     }
 }
