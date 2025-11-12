29f121045b4da76fba9e41ad168f3916a50d1b80
Matheus Kroeff <matheuslckroeff@gmail.com>
2025-11-12 08:48:34 -0300

    Update OperadorDrone.java

--- OperadorDrone.java
+++ OperadorDrone.java
@@ -1,1 +1,11 @@
+public class OperadorDrone extends Usuario {
 
+    public OperadorDrone(int id, String nome, String email) {
+        super(id, nome, email);
+    }
+
+    @Override
+    public boolean podeAgendarMissao() {
+        return true; 
+    }
+}
