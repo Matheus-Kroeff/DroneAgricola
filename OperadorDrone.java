126718e19e496d98fad2315e599ed2ca4df7dbac
Matheus Kroeff <matheuslckroeff@gmail.com>
2025-11-21 13:35:59 -0300

    Update OperadorDrone.java

--- OperadorDrone.java
+++ OperadorDrone.java
@@ -1,11 +1,22 @@
 public class OperadorDrone extends Usuario {
-
-    public OperadorDrone(int id, String nome, String email) {
-        super(id, nome, email);
+    
+    public OperadorDrone(int id, String nome, String email, String senhaPura) {
+        super(id, nome, email, senhaPura);
     }
 
+    // Implementação da Abstração: O Operador PODE agendar (retorna true)
     @Override
-    public boolean podeAgendarMissao() {
+    public boolean temPermissaoParaAgendar() {
         return true; 
+    }
+    
+    @Override
+    public String getTipoAcesso() {
+        return "Operador de Drone";
+    }
+    
+    // Método específico do Operador
+    public void executarVoo() {
+        System.out.println(getNome() + " está executando o voo.");
     }
 }
